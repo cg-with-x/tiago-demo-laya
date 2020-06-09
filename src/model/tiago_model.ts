75c752e41b1f121209dee0d9a20775e642c5cb07
Zhou Xinyu <zhouxinyu.1216@bytedance.com>
2020-06-09 13:15:56 +0800

    chore: add tiago  annotation

--- src/model/tiago_model.ts
+++ src/model/tiago_model.ts
@@ -1,5 +1,10 @@
 import { PlayerInfo } from "../../node_modules/@byted-creative/tiago/dist/interface";
 
+/**
+ * 存储tiago相关数据
+ *
+ * @class TiagoModel
+ */
 class TiagoModel{
   tiagoInit: boolean = false;
   selfUserInfo?: PlayerInfo;
