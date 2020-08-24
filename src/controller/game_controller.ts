d29c0962b474c59b19b28c8966bd97883c46990d
Zhou Xinyu <zhouxinyu.1216@bytedance.com>
2020-08-24 22:27:51 +0800

    feat: add single match record

--- src/controller/game_controller.ts
+++ src/controller/game_controller.ts
@@ -1,23 +1,30 @@
-import { gameModel } from '~/model/game_model';
-import { tiagoModel } from '~/model/tiago_model';
-import { gameScene } from '~/view/game_scene';
-import { mainScene } from '~/view/main_scene';
+import { gameModel } from "~/model/game_model";
+import { tiagoModel } from "~/model/tiago_model";
+import { gameScene } from "~/view/game_scene";
+import { mainScene } from "~/view/main_scene";
 
-import { roomController } from './room_controller';
-import tiago from '@byted-creative/tiago';
+import { roomController } from "./room_controller";
+import tiago from "@byted-creative/tiago";
+import recordController from './record_controller';
+import { tiagoController } from './tiago_controller';
 
-class GameController{
+class GameController {
   isGaming = false;
-  
+
   onClickEndGame() {
-      if (!roomController.room) return
-      roomController.room.send(JSON.stringify({
-          event: 'bye',
-      }));
+    // 停止录屏上传视频
+    recordController.recordManager.stop();
+    tiagoController.uploadVideo();
+    
+    if (!roomController.room) return;
+    roomController.room.send(
+      JSON.stringify({
+        event: "bye",
+      })
+    );
   }
 
-  onGameOver(){
-
+  onGameOver() {
     roomController.leaveRtcRoom();
     roomController.leave();
 
@@ -25,57 +32,58 @@ class GameController{
   }
 
   onClickTalkAttack() {
-    if (!roomController.room) return
-     
-    roomController.room.send(JSON.stringify({
-        event: 'talk',
-        data: Math.round(Math.random() * (100 - 0)) + 0,
-    }));
+    if (!roomController.room) return;
 
+    roomController.room.send(
+      JSON.stringify({
+        event: "talk",
+        data: Math.round(Math.random() * (100 - 0)) + 0,
+      })
+    );
   }
 
   onClickReconnect() {
     if (roomController.room) {
-        roomController.room.reconnect();
+      roomController.room.reconnect();
     }
   }
 
-  onRoomMessage(messageStr: string|ArrayBuffer) {
+  onRoomMessage(messageStr: string | ArrayBuffer) {
     // NOTE: 消息体的内容是开发者自己定义的，这里的代码只是一种示例
     // NOTE: 开发者可以根据自己的房间脚本和协议，实现自身游戏的逻辑
-    if (typeof(messageStr) !== "string") return;
+    if (typeof messageStr !== "string") return;
     const message = JSON.parse(messageStr);
 
     if (message.length) {
-      message.forEach(({ event, data}: {event:string, data:any}) => {
+      message.forEach(({ event, data }: { event: string; data: any }) => {
         switch (event) {
-          case 'game-start':
+          case "game-start":
+            recordController.recordManager.start({duration: 300});
             roomController.joinRtcRoom();
             break;
-          case 'environment':
+          case "environment":
             gameModel.environment = data;
             break;
-          case 'info':
+          case "info":
             gameModel.twoPlayersInfo = data;
             gameScene.renderPlayers();
             break;
-          case 'server-time':
+          case "server-time":
             gameModel.serverTime = data;
             gameScene.renderServerTime();
             break;
-          case 'talk':
+          case "talk":
             gameScene.renderTalk(data);
             break;
-          case 'game-over':
+          case "game-over":
             this.onGameOver();
             break;
           default:
-              break;
+            break;
         }
-      })
+      });
     }
   }
-
 }
 
 export const gameController = new GameController();
